2ecbad43f3bf5b256381c9bdfdc276c238498e47
mikias-tulu <miki.tulu@gmail.com>
2024-07-07 12:07:57 +0300

    SplitFeatureBlock: Add Implementation and mock data

--- slices/SplitFeatureBlock/index.tsx
+++ slices/SplitFeatureBlock/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
@@ -12,13 +13,32 @@ export type SplitFeatureBlockProps =
  */
 const SplitFeatureBlock = ({ slice }: SplitFeatureBlockProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for split_feature_block (variation:{" "}
-      {slice.variation}) Slices
-    </section>
+    <Flex direction="row" py="8" color="black" mt="20" px="10" bg="#178BFF" height="271px" align="center">
+      {slice.primary.split_feature_block.map((item, i) => (
+        <Box key={i} w="383.33px" h="131px" p="6" color="white" alignItems="left">
+          <Text
+            fontSize="62px"
+            fontWeight="700"
+            lineHeight="72px"
+            textAlign="left"
+            color="white"
+            mb="2"
+          >
+            {item.title}
+          </Text>
+          <Text
+            fontSize="18px"
+            fontWeight="400"
+            lineHeight="20px"
+            textAlign="left"
+            color="white"
+            marginTop="10px"
+          >
+            {item.description}
+          </Text>
+        </Box>
+      ))}
+    </Flex>
   );
 };
 
