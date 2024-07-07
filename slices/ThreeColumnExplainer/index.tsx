1f7993536d2847584097abfab89a487ec2ee0df6
mikias-tulu <miki.tulu@gmail.com>
2024-07-07 12:09:35 +0300

    ThreeColumnExplainer: Add Implementation and mock data

--- slices/ThreeColumnExplainer/index.tsx
+++ slices/ThreeColumnExplainer/index.tsx
@@ -1,5 +1,6 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `ThreeColumnExplainer`.
@@ -14,13 +15,41 @@ const ThreeColumnExplainer = ({
   slice,
 }: ThreeColumnExplainerProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for three_column_explainer (variation:{" "}
-      {slice.variation}) Slices
-    </section>
+    <Flex direction="row" py="8" color="black" mt="20" px="10" height="271px" align="start" gap="5">
+      <Box w="376px" p="6" py="0">
+        <Text fontSize="28px" lineHeight="36px" fontWeight="700" mb="2">
+          {slice.primary.title}
+        </Text>
+        <PrismicRichText field={slice.primary.description} />
+      </Box>
+      {slice.primary.multi_column_explainer.map((item, i) => (
+        <Box
+          key={i}
+          w="350px"
+          h="185px"
+          p="6"
+          color="white"
+          display="flex"
+          flexDirection="column"
+          justifyContent="start"
+          alignItems="flex-start"
+          bg="#3399FF"
+          borderRadius="8px"
+        >
+          <Text
+            fontSize="24px"
+            fontWeight="700"
+            lineHeight="28px"
+            textAlign="left"
+            color="white"
+            mb="2"
+          >
+            {item.title}
+          </Text>
+          <PrismicRichText field={item.description} />
+        </Box>
+      ))}
+    </Flex>
   );
 };
 
