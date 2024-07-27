11908163522011b9b09553174170258c5ea8f5f3
Hanna-Slesarska <slesarskaya.a@gmail.com>
2024-07-27 12:42:47 -0500

    update

--- slices/Testimonials/index.tsx
+++ slices/Testimonials/index.tsx
@@ -1,9 +1,9 @@
 
-import {Box, Grid, Container, Text, Center} from "@chakra-ui/react";
+import {Box, Grid, Heading, Container, Text, Center} from "@chakra-ui/react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
 import styles from './background.module.css'
-import { Heading }   from '../../app/components/Heading'
+//import { Heading }   from '../../app/components/Heading'
 
 /**
  * Props for `Testimonials`.
@@ -27,9 +27,9 @@ const Testimonials = ({ slice }: TestimonialsProps): JSX.Element => {
             <Heading as="h3" size="xl" mx="70">
               {slice.primary.title}
             </Heading>
-              {slice.primary.testimonials.map((item, i) => (
+              {slice.primary.testimonials.map(item => (
                 <Center pt="69">
-                  <Box bg={"white"} padding={70} key={i} width={{
+                  <Box bg={"white"} padding={70} width={{
                       sm: "calc(30% - 1.5rem * 2/3)",
                       lg: "calc(60% - 1.5rem * 2/3)"
                     }}>
