7a4d6760f891e0e36ee0779bafe5e93bc70fb7f6
Arvind Ganeshkumar <arvindram.ganesh@gmail.com>
2024-08-11 20:43:23 -0500

    fixing build

--- slices/HorizontalSlice/rightImage/index.tsx
+++ slices/HorizontalSlice/rightImage/index.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
 import { Box, Grid, GridItem } from "@chakra-ui/react";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicNextImage } from "@prismicio/next";
 import { BackgroundColor } from "@/app/components/BackgroundColor";
 import { ContainerWrapper } from "@/app/components/ContainerWrapper";
 import { TextBlockBottomButtonGroup } from "@/app/components/TextBlockBottomButtonGroup";
+
 const RightImage = (slice: Content.HorizontalSliceSlice): JSX.Element => {
   return (
     <BackgroundColor backgroundColor={slice.primary.background_color!}>
