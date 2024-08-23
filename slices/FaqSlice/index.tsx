55128ab8b44fe63188db30b787a6adaf3b13fb88
Arvind Ganeshkumar <arvindram.ganesh@gmail.com>
2024-08-23 18:53:17 -0400

    minor adjustments

--- slices/FaqSlice/index.tsx
+++ slices/FaqSlice/index.tsx
@@ -4,14 +4,15 @@ import { ContainerWrapper } from "@/app/components/ContainerWrapper";
 import { CustomHeading } from "@/app/components/CustomHeading";
 import { TextBlock } from "@/app/components/TextBlock";
 import {
-   Accordion,
-   AccordionButton,
-   AccordionIcon, 
-   AccordionItem, 
-   AccordionPanel, 
-   Box, 
-   Container, 
-   Flex,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Container,
+  Flex,
+  Stack,
 } from "@chakra-ui/react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
@@ -28,31 +29,26 @@ const FaqSlice = ({ slice }: FaqSliceProps): JSX.Element => {
   return (
     <BackgroundColor backgroundColor={slice.primary.backgroundcolor}>
       <ContainerWrapper>
-        <Flex direction="column" justify="center" align="center" color="black">
+        <Stack gap={"2.5rem"}>
           <Container p={0} textAlign={{ md: "center" }}>
             <TextBlock textBlock={slice.primary.heading_text_block} />
           </Container>
-
-          <Box width="100%" maxW="1000px" mt={"1.5rem"}>
-            <Accordion allowMultiple >
+          <Container p={0}>
+            <Accordion allowMultiple>
               {slice.primary.accordion.map((item, index) => (
                 <AccordionItem key={index}>
                   <AccordionButton>
                     <Box flex="1" textAlign="left">
-                      <CustomHeading as="h4">
-                        {item.heading}
-                      </CustomHeading>
+                      <CustomHeading as="h4">{item.heading}</CustomHeading>
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
-                  <AccordionPanel pb={4}>
-                    {item.description}
-                  </AccordionPanel>
+                  <AccordionPanel pb={4}>{item.description}</AccordionPanel>
                 </AccordionItem>
               ))}
             </Accordion>
-          </Box>
-        </Flex>
+          </Container>
+        </Stack>
       </ContainerWrapper>
     </BackgroundColor>
   );
